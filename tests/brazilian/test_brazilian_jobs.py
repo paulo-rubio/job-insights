d306f6bb0887491d3786a6c4e5b667f37f808b75
Rubio <paulocezarrubio@gmail.com>
2023-02-17 15:05:25 -0300

    <testing>(testing in brazillian)

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,11 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    brazilian_job = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    assert 'titulo' not in brazilian_job[0]
+    assert 'salário' not in brazilian_job[0]
+    assert 'tipo' not in brazilian_job[0]
+    assert 'title' in brazilian_job[0]
+    assert 'salary' in brazilian_job[0]
+    assert 'type' in brazilian_job[0]
