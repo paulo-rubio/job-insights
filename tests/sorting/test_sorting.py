eee678e916670529cc458c091594324862eb6f7e
Rubio <paulocezarrubio@gmail.com>
2023-02-17 15:26:23 -0300

    <testing sort>

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,23 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+from src.insights.jobs import read
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = read('data/jobs.csv')
+    sort_by(jobs, "min_salary")
+    assert jobs[-1]['min_salary'] == ""
+
+    sort_by(jobs, "min_salary")
+    assert jobs[0]['min_salary'] == '19857'
+
+    sort_by(jobs, "max_salary")
+    assert jobs[-1]['max_salary'] == ""
+
+    sort_by(jobs, "max_salary")
+    assert jobs[0]['max_salary'] == "383416"
+
+    sort_by(jobs, "date_posted")
+    assert jobs[-1]['date_posted'] == "2020-04-05"
+
+    sort_by(jobs, "date_posted")
+    assert jobs[0]['date_posted'] == "2020-05-08"
